9050663084289b391888881296e9ef26cd65cfa4
Shruti <98602069+lightangale@users.noreply.github.com>
2025-03-10 15:12:06 +0530

    Update Me.session.sql

--- Me.session.sql
+++ Me.session.sql
@@ -10,31 +10,15 @@ CREATE TABLE tasks (
     tasks_completed_today INT DEFAULT 0,
     total_tasks INT DEFAULT 0
 );
-
-INSERT INTO habits (date, habits_completed_today, total_habits) 
-VALUES ('2025-03-05', 6, 6);
-
-INSERT INTO tasks (date, tasks_completed_today, total_tasks) 
-VALUES ('2025-03-05', 5, 10);
-
-SELECT * FROM habits;
-SELECT * FROM tasks;
-
--- The below tables will be used for training the model
-
-CREATE TABLE habit_logs (
+-- The below table is used for the mood tracker
+CREATE TABLE mood (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    habit_name VARCHAR(255),
-    completed BOOLEAN DEFAULT 0,
-    time_completed TIME,
-    day_completed VARCHAR(10)
-);
-
-CREATE TABLE task_logs (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    task_name VARCHAR(255),
-    completed BOOLEAN DEFAULT 0,
-    time_completed TIME,
-    day_completed VARCHAR(10)
+    date VARCHAR(10) ,
+    Mood_Rating INT,
+    Feelings TEXT,
+    Satisfaction_Reason TEXT,
+    Activities TEXT,
+    Interesting_Event TEXT,
+    Self_Care TEXT
 );
 
