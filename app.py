95d4a4839d0a706adbe392be473bc5b174e2ebc8
Shruti <98602069+lightangale@users.noreply.github.com>
2025-03-20 01:42:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -340,4 +340,5 @@ def gemini_summary():
     
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render assigns a port dynamically
+    app.run(host="0.0.0.0", port=port, debug=True)
